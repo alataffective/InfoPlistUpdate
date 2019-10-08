6178b070594fb016740c9db88b2df625c120a66b
Alan Fransham <alataffective@users.noreply.github.com>
2019-10-08 10:17:00 +0100

    Increment CFBundleVersion

---
+++ InfoPlistUpdate/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace InfoPlistUpdate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                ShowUsage();
+                return;
+            }
+
+            var filePath = args[0];
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($"The path {filePath} does not exist.");
+                return;
+            }
+
+            var xmlDocument = new XmlDocument();
+            xmlDocument.Load(filePath);
+
+            IncrementBundleVersion(xmlDocument, filePath);
+        }
+
+        static void IncrementBundleVersion(XmlDocument xmlDocument, string filePath)
+        {
+            var bundleVersionKeyNode = xmlDocument.SelectSingleNode("/plist/dict/key[text()='CFBundleVersion']");
+            var bundleVersionValueNode = bundleVersionKeyNode.NextSibling;
+            if (bundleVersionValueNode.Name != "string")
+            {
+                Console.WriteLine("string node expected following CFBundleVersion key");
+            }
+
+            if (!int.TryParse(bundleVersionValueNode.InnerText, out var bundleVersionValue))
+            {
+                Console.WriteLine($"Cannot parse {bundleVersionValueNode.InnerText} as an integer");
+            }
+
+            ++bundleVersionValue;
+
+            bundleVersionValueNode.InnerText = bundleVersionValue.ToString();
+
+            xmlDocument.Save(filePath);
+        }
+
+        static void ShowUsage()
+        {
+            Console.WriteLine("Usage: InfoPlistUpdate <Info.plist file path>");
+        }
+    }
+}
